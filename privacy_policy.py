28b928d3e66d4780c1f9baa8e2fdd3a2d0b21f0b
vladvlad2525 <144328548+vladvlad2525@users.noreply.github.com>
2023-09-12 15:44:01 +0300

    Update privacy_policy.py

--- privacy_policy.py
+++ privacy_policy.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from actions import click_button_by_xpath
-from webdriver_set import driver
+
 
 def privacy_policy(driver):
     click_button_by_xpath(driver, '//*[@id="block-8"]/ul/li[3]/a' )
